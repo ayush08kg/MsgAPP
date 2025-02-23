674aa7c53097617b4077f7eafdd7dd1a5375cbfb
Akg <ayushkrgupta2003@gmail.com>
2025-02-23 22:34:20 +0530

    ready-for-production

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import path from "path";
 import cors from "cors"
 import { app , server } from "./lib/socket.js";
 
@@ -11,6 +12,8 @@ dotenv.config();
 
 
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -24,6 +27,16 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname , "../frontend","dist","index.html"));
+    })
+}
+
 
 server.listen(PORT,()=>{
     console.log("Server is running on port PORT:" + PORT);
